b5988bb4cd46472a767ca9d04ace0cadfa386aa3
vinay <vinayveeren@gmail.com>
2018-04-22 16:02:55 +0530

    Update config.js
    config file changed

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 module.exports  = {
     secretKey : "superSecrete",
     mongoUri : "mongodb://localhost:27017/auth-app",
-    mlab:'mongodb://admin:admin@ds215709.mlab.com:15709/feedback-system'
+    mlab:'place your url'
 }
